c5703576dda7579877dace57a227733b40f0d27f
mikebusby <me@mikebusby.ca>
2018-01-18 16:47:11 -0500

    Added in SVG icon generation

--- gulpfile.js
+++ gulpfile.js
@@ -15,32 +15,33 @@
 // ++ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ++
 
 // Require Gulp & Plugins
-var gulp            = require("gulp");
-var gulpLoadPlugins = require("gulp-load-plugins");
+var gulp            = require('gulp');
+var path            = require('path');
+var gulpLoadPlugins = require('gulp-load-plugins');
 var bourbon         = require('node-bourbon').includePaths;
 
 // Rename some plugins
 var plugins = gulpLoadPlugins({
   rename: {
-    "gulp-sass": "sass",
-    "gulp-bower": "bower",
-    "gulp-file-include": "fileinclude",
-    "gulp-autoprefixer": "autoprefixer"
+    'gulp-sass': 'sass',
+    'gulp-bower': 'bower',
+    'gulp-file-include': 'fileinclude',
+    'gulp-autoprefixer': 'autoprefixer'
   }
 });
 
 // Config Variables
 var config = {
-  srcPath:   "src/",
-  buildPath: "www/",
-  tplPath:   "src/tpl/"
+  srcPath:   'src/',
+  buildPath: 'www/',
+  tplPath:   'src/tpl/'
 }
 
 // Output errors to console
 function errorLog(error) {
   console.error.bind(error);
   plugins.notify().write(error);
-  this.emit("end");
+  this.emit('end');
 }
 
 
@@ -49,7 +50,7 @@ function errorLog(error) {
 // HTML Compliation
 //
 gulp.task('html', function() {
-  gulp.src([config.tplPath + "*.html"])
+  gulp.src([config.tplPath + '*.html'])
     .pipe(plugins.fileinclude({
       prefix: '@@',
       basepath: '@file'
@@ -62,12 +63,12 @@ gulp.task('html', function() {
 //
 // JavaScript Taks
 //
-gulp.task("scripts", function() {
-  gulp.src([config.srcPath + "js/*.js", !config.srcPath + "www/js/*.min.js"])
+gulp.task('scripts', function() {
+  gulp.src([config.srcPath + 'js/*.js', !config.srcPath + 'www/js/*.min.js'])
     .pipe(plugins.uglify())
-    .on("error", errorLog)
-    .pipe(plugins.concat("main.min.js"))
-    .pipe(gulp.dest(config.buildPath + "js"));
+    .on('error', errorLog)
+    .pipe(plugins.concat('main.min.js'))
+    .pipe(gulp.dest(config.buildPath + 'js'));
 });
 
 
@@ -75,20 +76,52 @@ gulp.task("scripts", function() {
 //
 // SCSS Compliation
 //
-gulp.task("styles", function() {
-  gulp.src(config.srcPath + "scss/*.scss")
+gulp.task('styles', function() {
+  gulp.src(config.srcPath + 'scss/*.scss')
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass({
-      outputStyle: "compressed",
-      includePaths: ["styles"].concat(bourbon)
+      outputStyle: 'compressed',
+      includePaths: ['styles'].concat(bourbon)
     }))
     .pipe(plugins.autoprefixer({
-       browsers: ["last 3 versions"],
+       browsers: ['last 3 versions'],
        cascade: false
      }))
-    .pipe(plugins.sourcemaps.write("/"))
-    .on("error", errorLog)
-    .pipe(gulp.dest(config.buildPath + "css"));
+    .pipe(plugins.sourcemaps.write('/'))
+    .on('error', errorLog)
+    .pipe(gulp.dest(config.buildPath + 'css'));
+});
+
+
+
+//
+// SVG Icon Generation
+//
+gulp.task('svgicons', function () {
+  var svgs = gulp
+    .src(config.srcPath + 'icons/*.svg')
+    .pipe(plugins.svgmin(function (file) {
+      var prefix = path.basename(file.relative, path.extname(file.relative));
+      return {
+        plugins: [{
+          cleanupIDs: {
+            prefix: prefix + '-',
+            minify: true
+          }
+        }]
+      }
+    }))
+    .pipe(plugins.svgstore({ inlineSvg: true }));
+
+  function fileContents(filePath, file) {
+    return file.contents.toString();
+  }
+
+  return gulp
+    .src(config.srcPath + 'icons/_inline-icons.html')
+    .pipe(plugins.inject(svgs, { transform: fileContents }))
+    .pipe(gulp.dest(config.srcPath + 'tpl/'));
+
 });
 
 
@@ -96,7 +129,7 @@ gulp.task("styles", function() {
 //
 // Web Server
 //
-gulp.task("webserver", function() {
+gulp.task('webserver', function() {
   gulp.src(config.buildPath)
     .pipe(plugins.webserver({
       port: 1337,
@@ -109,9 +142,9 @@ gulp.task("webserver", function() {
 //
 // Move Images to build
 //
-gulp.task("copyimg", function() {
-  gulp.src(config.srcPath + "/img/*")
-  .pipe(gulp.dest(config.buildPath + "img/"));
+gulp.task('copyimg', function() {
+  gulp.src(config.srcPath + '/img/*')
+  .pipe(gulp.dest(config.buildPath + 'img/'));
 });
 
 
@@ -119,9 +152,9 @@ gulp.task("copyimg", function() {
 //
 // Move Bower pkg's to build (some needed)
 //
-gulp.task("copyvendor", function() {
-  gulp.src("vendor/**/*")
-  .pipe(gulp.dest(config.buildPath + "vendor/"));
+gulp.task('copyvendor', function() {
+  gulp.src('vendor/**/*')
+  .pipe(gulp.dest(config.buildPath + 'vendor/'));
 });
 
 
@@ -129,12 +162,13 @@ gulp.task("copyvendor", function() {
 //
 // Watch File changes
 //
-gulp.task("watch", function() {
-  gulp.watch(config.srcPath + "img/*", ["copyimg"]);
-  gulp.watch(config.srcPath + "vendor/*", ["copyvendor"]);
-  gulp.watch(config.srcPath + "js/*.js", ["scripts"]);
-  gulp.watch(config.srcPath + "scss/**/*.scss", ["styles"]);
-  gulp.watch(config.tplPath + "**/*.html", ["html"]);
+gulp.task('watch', function() {
+  gulp.watch(config.srcPath + 'img/*', ['copyimg']);
+  gulp.watch(config.srcPath + 'vendor/*', ['copyvendor']);
+  gulp.watch(config.srcPath + 'js/*.js', ['scripts']);
+  gulp.watch(config.srcPath + 'scss/**/*.scss', ['styles']);
+  gulp.watch(config.srcPath + 'icons/*.svg', ['svgicons']);
+  gulp.watch(config.tplPath + '**/*.html', ['html']);
 });
 
 
@@ -142,12 +176,13 @@ gulp.task("watch", function() {
 //
 // Run Tasks | $ gulp
 //
-gulp.task("default", [
-  "html",
-  "scripts",
-  "styles",
-  "copyimg",
-  "copyvendor",
-  "watch",
-  "webserver"
+gulp.task('default', [
+  'html',
+  'scripts',
+  'styles',
+  'svgicons',
+  'copyimg',
+  'copyvendor',
+  'watch',
+  'webserver'
 ]);
