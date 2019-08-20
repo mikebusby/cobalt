c2b7c456f6c63438dc168dabdf6cc313f4cdb63f
mikebusby <hello@mikebusby.email>
2019-08-19 21:31:12 -0400

    Switched out gulp-webserver for browserSync

--- build/server.js
+++ build/server.js
@@ -2,40 +2,22 @@
 // Localhost server task
 //
 
+const browserSync = require('browser-sync');
+
 module.exports = function(gulp, plugins, config) {
   return function() {
     const stream = 
-      gulp.src(config.buildPath)
-        .pipe(plugins.webserver({
-          middleware: function (req, res, next) {
-            if (
-              req.url.indexOf('css') >= 0 ||
-              req.url.indexOf('js') >= 0 ||
-              req.url.indexOf('img') >= 0 ||
-              req.url.indexOf('favicon.ico') >= 0
-            ) {
-              next();
-              return
-            }
-
-            // if is a request for index in a folder
-            if (req.url.slice(-2).length > 1 && req.url.slice(-1) === '/') {
-              req.url = req.url + 'index';
-            }
-
-            // if root page
-            if (req.url === '/') {
-              req.url = '/index';
-            }
-
-            req.url = req.url + '.html';
-            next();
-          },
-          fallback: '404.html',
-          host: '0.0.0.0',
-          port: 1337,
-          livereload: true
-        }));
+      browserSync.init({
+        server: {
+          baseDir: './www',
+          serveStaticOptions: {
+            extensions: ['html']
+          }
+        },
+        open: false,
+        port: 1337,
+        watch: true,
+      });
     
     return stream;
   }
