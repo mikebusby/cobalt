179c160adba115ba5d697623e03c7280f480600f
mikebusby <mike@busby.design>
2021-01-08 19:53:49 -0500

    Small tailwindcss config clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        default: '1rem',
+        DEFAULT: '1rem',
         sm: '2rem',
         lg: '4rem',
         xl: '5rem',
